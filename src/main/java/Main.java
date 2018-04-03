a4d091114fdf337e3d044628087be89793aae83f
Viktoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-04-03 15:04:10 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class Main {
+
+    private static String str;
+    int a;
+    int b;
+    int c;
+    Map<String, Integer> map;
+    List<Object> list;
+    //   Cursor cursor;
+
+}
